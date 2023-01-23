cbe40c5193c32cd4b67750571c60615dea23acad
lbonif92 <loic.boniface@gmail.com>
2023-01-23 17:39:20 +0100

    add environnement vars

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 //jshint esversion:6
 
+require('dotenv').config()
 const express = require("express");
 const port = 3000;
 const bodyParser = require("body-parser");
@@ -7,6 +8,8 @@ const ejs = require("ejs");
 var mongoose = require('mongoose');
 var encrypt = require('mongoose-encryption');
 const app = express();
+
+console.log(process.env.API_KEY);
 
 app.use(express.static("public"));
 app.set('view engine', 'ejs');
@@ -21,8 +24,7 @@ const userSchema = new mongoose.Schema ({
     password: String
 });
 
-const secret = "Thisisourlittlesecret.";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ['password'] });
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 const User = new mongoose.model("User", userSchema);
 
